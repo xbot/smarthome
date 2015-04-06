2db46b26af4f42063ce92802e0e1874ef67ff786
Donie Leigh <donie.leigh@gmail.com>
2015-04-06 15:12:21 +0000

    use yapbl

--- script/alert.py
+++ script/alert.py
@@ -10,21 +10,15 @@
 ####################################################
 
 import sys, getopt, time
-from pushbullet import Pushbullet
+from yapbl import PushBullet
 
 API_KEY = ''
 
 def send_alert(image):
-    pb = Pushbullet(API_KEY)
-
+    pb = PushBullet(API_KEY)
     timePoint = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     pb.push_note('入侵警报', timePoint + '，发现入侵者！！！')
-
-    with open(image, "rb") as pic:
-        file_data = pb.upload_file(pic, "picture.jpg")
-
-    push = pb.push_file(**file_data)
-    return push
+    pb.push_file(open(image, 'rb'))
 
 if __name__ == '__main__':
     opts, args = getopt.getopt(sys.argv[1:], "f:")
