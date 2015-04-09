2c1592f9c6e2b6f6db77678e9f831d79fa79aab5
Donie Leigh <donie.leigh@gmail.com>
2015-04-09 16:27:18 +0800

    return statuses

--- home/main.py
+++ home/main.py
@@ -15,6 +15,7 @@ import os
 import time
 import types
 import json
+import commands
 from ConfigParser import ConfigParser, NoSectionError, NoOptionError
 from pushbullet import Pushbullet, Listener, PushbulletError
 from systemd import journal
@@ -31,28 +32,36 @@ class SmartHome:
     def __init__(self, cfg):
         self.cfg = cfg
         self.lastFetch = time.time()
+        self.apiKey = self.cfg.get('global', 'api_key')
+        self.pb = Pushbullet(self.apiKey)
 
-    def followCommand(self, cmd):
-        if cmd['command'] == 'start_motion':
+    def followCommand(self, cmd, fromIden=None):
+        if cmd['command'] == 'get_status':
+            fromDevice = self.getDevice(fromIden)
+            if fromDevice is None:
+                journal.send('get_status: Cannot find device with iden "' + fromIden + '"', PRIORITY=journal.LOG_ERR)
+                return
+            status = {'command':'get_status', 'data':{}}
+            (tmpStatus, tmpOutput) = commands.getstatusoutput('systemctl status motion')
+            status['data']['motion'] = tmpStatus == 0 and 'on' or 'off'
+            self.pb.push_note('status', json.dumps(status), fromDevice)
+        elif cmd['command'] == 'start_motion':
             os.system('systemctl start motion')
         elif cmd['command'] == 'stop_motion':
             os.system('systemctl stop motion')
 
     def run(self):
-        apiKey = self.cfg.get('global', 'api_key')
-        pb = Pushbullet(apiKey)
-
         try:
             deviceIden = self.cfg.get('global', 'device_iden')
         except NoOptionError:
-            deviceIden = pb.new_device('SmartHome').device_iden
+            deviceIden = self.pb.new_device('SmartHome').device_iden
             self.cfg.set('global', 'device_iden', deviceIden)
             with open(CONFIG_FILE, 'w') as f:
                 self.cfg.write(f)
 
         def on_push(msg):
             try:
-                pushes = pb.get_pushes(self.lastFetch)
+                pushes = self.pb.get_pushes(self.lastFetch)
                 self.lastFetch = time.time()
                 if type(pushes) is types.TupleType and len(pushes)>1 \
                         and type(pushes[1]) is types.ListType:
@@ -60,12 +69,19 @@ class SmartHome:
                         if msg.has_key('target_device_iden') and msg['target_device_iden'] == deviceIden:
                             cmd = json.loads(msg['body'])
                             journal.send('Follow command: ' + msg['body'])
-                            self.followCommand(cmd)
+                            self.followCommand(cmd, fromIden=msg['source_device_iden'])
             except (PushbulletError, IOError, ValueError, KeyError), e:
                 journal.send(e.message, PRIORITY=journal.LOG_ERR)
 
-        lsr = Listener(Account(apiKey), on_push)
+        lsr = Listener(Account(self.apiKey), on_push)
         lsr.run()
+
+    def getDevice(self, iden):
+        if type(iden) in [types.StringType, types.UnicodeType]:
+            for device in self.pb.devices:
+                if device.device_iden == iden:
+                    return device
+        return None
 
 
 if __name__ == '__main__':
@@ -83,3 +99,4 @@ if __name__ == '__main__':
         sys.exit(1)
     except KeyboardInterrupt:
         print('Game over.')
+
